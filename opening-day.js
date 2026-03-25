bb63388521eff8950e799f10438b136f586d0fd2
Sherlyne Hernandez <sherlyne625@gmail.com>
2026-03-25 16:35:19 -0500

    Add solution to opening-day.js

---
+++ opening-day.js
@@ -0,0 +1,26 @@
+function streakCounter(games) {
+  let counter = 0;
+  let longestStreak = 0;
+
+  for (let game of games) {
+    if (game === "W") {
+      counter++;
+      longestStreak = Math.max(longestStreak, counter);
+    } else if (game === "L") {
+      counter = 0;
+    }
+  }
+  
+  return longestStreak;
+}
+
+// Inputs:
+
+// Example 1
+console.log(streakCounter(["W", "W", "L", "W", "W", "W", "L", "W", "W"])); // Output: 3
+
+// Example 2
+console.log(streakCounter(["W", "W", "R", "W", "W", "L", "W"])); // Output: 4
+
+// Example 3
+console.log(streakCounter(["R", "R", "W", "R", "R", "W", "W", "R", "R", "W", "W", "W", "R"])); // Output: 6
